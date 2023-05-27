6e19a05a820ac72d8dec6c37737a45edc2ad8676
Venkat Karanam <karanamvasu871@gmail.com>
2023-05-27 23:41:02 +0530

    Portfolio page

---
+++ src/utils/workContributionsData.js
@@ -0,0 +1,90 @@
+const imgResourcePath = '/designer_portfolio/assets/images/projects/';
+
+const workContributions = [
+    {
+        company: 'Internshala',
+        contribution: {
+            title: 'Personalized Payment Plans',
+            info: 'Subscription That Fits Your Recruitment'
+        },
+        achievements: [
+            {
+                title: 'Monthly Revenue',
+                value: '8Lakh+'
+            },
+            {
+                title: 'Monthly Leads',
+                value: '400+'
+            }
+        ],
+        workSampleImg: imgResourcePath + 'Internshala.png',
+        category: 'Product Design',
+        cardTheme: '#CCDCED'
+    },
+    {
+        company: 'Slog Digital',
+        contribution: {
+            title: 'Revitalize Your Business',
+            info: 'Revamping Website for Brand Awareness'
+        },
+        achievements: [
+            {
+                title: 'Customer Acquisition',
+                value: '200%'
+            },
+            {
+                title: 'Increased User Traffic',
+                value: '40%'
+            }
+        ],
+        workSampleImg: imgResourcePath + 'SlogDigital.png',
+        category: 'Product Design',
+        cardTheme: '#DEF6E7'
+    },
+    {
+        company: 'Internshala',
+        contribution: {
+            title: 'Discover Your Next Opportunity',
+            info: 'Find Nearby Internships on the Map'
+        },
+        achievements: [],
+        workSampleImg: imgResourcePath + 'Mapview.png',
+        category: 'Concept Design',
+        cardTheme: '#FFFFFF'
+    },
+    {
+        company: 'Simply Life',
+        contribution: {
+            title: 'Reliable Maid Services',
+            info: 'Trustworthy Maids at Your Fingertips'
+        },
+        achievements: [],
+        workSampleImg: imgResourcePath + 'Simplylife.png',
+        category: 'Concept Design',
+        cardTheme: '#F9EB7F'
+    },
+    {
+        company: 'Internshala',
+        contribution: {
+            title: 'Never Miss a Moment',
+            info: 'Delicious Snacks at Your Fingertips'
+        },
+        achievements: [],
+        workSampleImg: imgResourcePath + 'Snacky.png',
+        category: 'Concept Design',
+        cardTheme: '#E84C4F'
+    },
+    {
+        company: 'Lalassa',
+        contribution: {
+            title: 'Redefining user experience for the',
+            info: 'Indian petcare industry with Lalassa Miss a Moment'
+        },
+        achievements: [],
+        workSampleImg: imgResourcePath + 'Lalassa.png',
+        category: 'Coming Soon...',
+        cardTheme: '#FF9A40'
+    },
+];
+
+export default workContributions;
